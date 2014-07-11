d7b6e40bc01f0065712fd485148e067c6cc9ad75
leleliu008 <792793182@qq.com>
2014-07-11 21:57:49 +0800

    删除无用的可执行文件

--- macro/build_in_macro.c
+++ macro/build_in_macro.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define DATE_TIME __DATE__" "__TIME__
+
 void func() {
-	printf("release date=%s, time=%s, fileName=%s, line=%d, fuction=%s\n",\
-			__DATE__, __TIME__, __FILE__, __LINE__, __func__);
+	printf("release date_time=%s, file=%s, line=%d, fuction=%s\n",\
+			DATE_TIME, __FILE__, __LINE__, __func__);
 }
 
 int main() {
 	func();
-	printf("release date=%s, time=%s, fileName=%s, line=%d, fuction=%s\n",\
-			__DATE__, __TIME__, __FILE__, __LINE__, __func__);
+	printf("release date_time=%s, file=%s, line=%d, fuction=%s\n",\
+			DATE_TIME, __FILE__, __LINE__, __func__);
 	return 0;
 }
