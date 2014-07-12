0eaea9b5bcbeca36a27c5f22cf446fa9da5d8754
leleliu008 <792793182@qq.com>
2014-07-12 22:35:45 +0800

    增加随机数函数的测试

---
+++ number/stdlib/rand.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main() {
+	printf("RAND_MAX = %d\n", RAND_MAX);
+
+	//产生随机种子
+	srand(time(NULL));
+
+	int i, j;
+	for(i = 0; i < 10; i++) {
+		j = 10.0 * rand() / RAND_MAX;
+		printf("%d \n", j);
+	}
+	return 0;
+}
