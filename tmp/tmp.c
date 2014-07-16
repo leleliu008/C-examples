42add803b19fe152812548a93c0392760a5e4917
leleliu008 <792793182@qq.com>
2014-07-16 14:41:26 +0800

    添加一个字符的测试

---
+++ tmp/tmp.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+	char a = 'x';
+	char b = 'y';
+
+	printf("a = %c\n", a);
+	printf("a = %d\n", a);
+	printf("b = %c\n", b);
+	printf("b = %d\n", b);
+
+	return 0;
+}
