16de5812f2a18aa4cbcab8ea6565c7adec307361
leleliu008 <792793182@qq.com>
2014-07-28 01:24:01 +0800

    添加数组测试

---
+++ array/array5.c
@@ -0,0 +1,17 @@
+/* 字符数组测试 */
+
+int main() {
+	char a[10] = {'c', ' ', 'p', 'r', 'o', 'g', 'r', 'a', 'm'};
+	char b[] = {'c', ' ', 'p', 'r', 'o', 'g', 'r', 'a', 'm'};
+	char c[] = "c program";
+	char d[10] = "c program";
+	char e[9] = "c program";
+
+	char ch = 'A';
+	char* p1 = &ch;
+	char* p2 = a;
+
+	/* 此处打断点 */
+
+	return 0;
+}
