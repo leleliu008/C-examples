44cf81714097b8a0a66abca0a6035f8cc9ee8750
leleliu008 <792793182@qq.com>
2014-08-03 17:51:00 +0800

    修该带参数的main函数的使用示例

--- function/main4.c
+++ function/main4.c
@@ -1,5 +1,12 @@
 /* main函数参数测试 */
 
+#include<stdio.h>
+
 int main(int argc, char *argv[], char *env[]) {
+	int i = 0;
+	while (NULL != env[i]) {
+		printf("%s\n", env[i]);
+		i++;
+	}
 	return 0;
 }
