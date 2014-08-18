f411fc0d88ec795d651bb869996bd5963083e1b5
leleliu008 <792793182@qq.com>
2014-08-18 18:06:32 +0800

    添加ftell函数测试

---
+++ stdio/ftell.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+	FILE *fp = fopen("ftell.c", "r");
+	if(fp) {
+		if(0 == fseek(fp, 0L, SEEK_END)) {
+			long length = ftell(fp);
+			printf("ftell.c is %ld Byte", length);
+		}
+
+		fclose(fp);
+	}
+	return 0;
+}
