86c32d2022e510dfdfd9b18a5b9958e4a216cddd
leleliu008 <792793182@qq.com>
2014-08-20 05:44:04 +0800

    添加对assert.h头文件的测试

---
+++ assert/assert.c
@@ -0,0 +1,12 @@
+#include<assert.h>
+#include<stdio.h>
+
+int main() {
+	int i = 0;
+
+	assert(i);
+
+	printf("assert success\n");
+	
+	return 0;
+}
---
+++ assert/assert2.c
@@ -0,0 +1,14 @@
+#define NDEBUG
+
+#include<assert.h>
+#include<stdio.h>
+
+int main() {
+	int i = 0;
+
+	assert(i);
+
+	printf("assert success\n");
+	
+	return 0;
+}
